f9e034fa06d5b2f64de4c7d5f55505c8379822bd
rmmenezes <ra29fa@gmail.com>
2022-02-15 16:48:29 -0300

    .

--- compute.tf
+++ compute.tf
@@ -3,7 +3,23 @@ resource "libvirt_volume" "compute-qcow2" {
   name = "compute.qcow2"
   pool = "default" # List storage pools using virsh pool-list
   source = "./images/focal-server-cloudimg-amd64.img"
-  format = "img"
+  format = "qcow2"
+}
+
+data "template_file" "user_data_compute" {
+  template = file("${path.module}/cloud_init/cloud_init_compute.cfg")
+}
+
+data "template_file" "network_config_compute" {
+  template = file("${path.module}/cloud_init/network_config_compute.cfg")
+}
+
+
+resource "libvirt_cloudinit_disk" "commoninit_compute" {
+  name           = "commoninit_compute.iso"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+  pool           = "default"
 }
 
 # Define KVM domain to create
@@ -11,6 +27,9 @@ resource "libvirt_domain" "compute" {
   name   = "compute"
   memory = "12288"
   vcpu   = 12
+
+  cloudinit = libvirt_cloudinit_disk.commoninit_compute.id
+
 
   network_interface {
     network_name = "default" # List networks with virsh net-list
--- controller.tf
+++ controller.tf
@@ -3,16 +3,30 @@ resource "libvirt_volume" "controller-qcow2" {
   name = "controller.qcow2"
   pool = "default" # List storage pools using virsh pool-list
   source = "./images/focal-server-cloudimg-amd64.img"
-  format = "img"
+  format = "qcow2"
 }
 
-data "template_file" "user_data" {
-  template = file("${path.module}/cloud_init.cfg")
+# Defining VM Volume
+resource "libvirt_volume" "controller-2-qcow2" {
+  name = "controller-2.qcow2"
+  pool = "default" # List storage pools using virsh pool-list
+  format = "qcow2"
 }
 
-resource "libvirt_cloudinit_disk" "commoninit" {
-  name           = "commoninit.iso"
+
+data "template_file" "user_data_controller" {
+  template = file("${path.module}/cloud_init/cloud_init_controller.cfg")
+}
+
+data "template_file" "network_config_controller" {
+  template = file("${path.module}/cloud_init//network_config_controller.cfg")
+}
+
+
+resource "libvirt_cloudinit_disk" "commoninit_controller" {
+  name           = "commoninit_controller.iso"
   user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
   pool           = "default"
 }
 
@@ -23,7 +37,7 @@ resource "libvirt_domain" "controller" {
   memory = "12288"
   vcpu   = 12
 
-  cloudinit = libvirt_cloudinit_disk.commoninit.id
+  cloudinit = libvirt_cloudinit_disk.commoninit_controller.id
 
   network_interface {
     network_name = "default" # List networks with virsh net-list
